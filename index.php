052950851dc374b59d471f1ad9d6019cc7ebd1e8
DiegoCloud <diegolajusticiasujas@icloud.com>
2025-04-27 20:59:38 +0200

    php - Turn left

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 
 class Rover
 {
-    private const DIRECTIONS = ["N","E","S","W"];
     private int $x;
     private int $y;
     private string  $direction;
+    private const DIRECTIONS = ["N", "E", "S", "W"];
 
     // Methods -> source : https://www.php.net/manual/en/language.oop5.decon.php
     function __construct(int $x, int $y, string $direction) {
@@ -18,14 +18,22 @@ class Rover
 
     // Turn left -> source : https://www.php.net/manual/en/function.array-search.php
     function turn_left() {
-        # Find current index inside DIRECTIONS
-        # Find the new index inside DIRECTIONS
+        # Find current index inside directions
+        $current_index = array_search($this->direction, self::DIRECTIONS);
+
+        # Find new index inside directions
+        $new_index = $current_index - 1;
+
+        # Logic to get new direction when negative index is found.
+        if ($new_index < 0) {
+            $new_index = count(self::DIRECTIONS) -1;
+        }
+       
         # Update DIRECTION
+        $this->direction = self::DIRECTIONS[$new_index];
     }
     function turn_right() {
-        # Find current index inside DIRECTIONS
-        # Find the new index inside DIRECTIONS
-        # Update DIRECTION
+
     }
 
 }
