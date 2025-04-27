bc7c980060b3511df0462a0eac9a26a4290b9c04
DiegoCloud <diegolajusticiasujas@icloud.com>
2025-04-27 13:35:10 +0200

    turn_right() test ✅

--- test.py
+++ test.py
@@ -3,5 +3,9 @@ from script import Rover
 print("Test 1 - Turn left & Turn Right")
 
 r = Rover(0,0,'N')
+
 r.turn_right()
 print(r.direction) # Expected output : "E"
+
+r.turn_right()
+print(r.direction) # Expected output : "S"
