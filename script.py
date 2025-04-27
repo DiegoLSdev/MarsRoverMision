4f3d4ea192e0e755766007eff8c39fd689d08603
DiegoCloud <diegolajusticiasujas@icloud.com>
2025-04-27 13:17:57 +0200

    turn_left()

--- script.py
+++ script.py
@@ -5,3 +5,14 @@ class Rover :
         self.x = x
         self.y = y
         self.direction = direction
+
+def turn_left(self):
+    current_direction_index = DIRECTIONS.index(self.direction)
+    new_direction_index = current_direction_index - 1 # -1 because turn is to left
+
+    # Logic to get new direction when negative index is found.
+    if new_direction_index < 0 :
+        new_direction_index = len(DIRECTIONS) - 1 # new_direction_index = 4 - 1 -> "W"
+
+    new_direction = DIRECTIONS[new_direction_index]
+    self.direction = new_direction
