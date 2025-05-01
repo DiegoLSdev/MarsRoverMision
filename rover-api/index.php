f71f689ab2704ac291971576ddaf505e8b84b36b
DiegoCloud <diegolajusticiasujas@icloud.com>
2025-05-01 15:09:55 +0200

    Routes logic

---
+++ rover-api/index.php
@@ -0,0 +1,16 @@
+<?php
+
+# Declare index.php
+
+# Jsoon file persist
+
+# Read route and method
+
+# Conditional logic to act according the action (route)
+    #  POST -> api/rover/start
+    #  POST -> api/rover/execute_commands
+    #  GET  -> api/rover/status
+    #  POST -> api/rover/restart
+
+
+?>
