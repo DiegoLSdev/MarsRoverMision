85ea0737c879d829c09a986a3a05894ecb668bc7
DiegoCloud <diegolajusticiasujas@icloud.com>
2025-05-03 15:17:42 +0200

    execute_commands

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import GetRoverStatus from './components/GetRoverStatus.vue'
 import StartRover from './components/startRover.vue'
+import RoverCommands from './components/RoverCommands.vue'
 </script>
 
 <template>
@@ -9,21 +10,10 @@ import StartRover from './components/startRover.vue'
     <div>
       <StartRover />
       <GetRoverStatus />
+      <RoverCommands />
     </div>
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ client/src/components/RoverCommands.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+import { executeCommands } from '../api/rover';
+
+const commands = ref(null);
+
+async function onExecuteCommands() {
+  try {
+    const response = await executeCommands(commands.value);
+    console.log('Commands executed successfully:', response);
+    } catch (error) {
+    console.log('Error executing commands:', error);
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <h2>Execute Commands</h2>
+    <div>
+        <label for="">Commands</label>
+        <input v-model="commands" type="text" placeholder="Enter commands (e.g., FFLFFR)" />
+        <button @click="onExecuteCommands"> Execute Commands</button>
+        {{ commands }}
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
